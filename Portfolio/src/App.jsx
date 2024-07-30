58dd87e9b5f7c2639062147dd344210220981363
Hamad Hafed <hafedhamad23@gmail.com>
2024-07-30 10:15:13 +0000

    ortfolio

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import Header from './component/1-Header/header'
+import Hero from './component/2-Hero/hero'
+import Main from './component/3-Main/Main'
+import Contact from './component/4-Contact/Contact'
+import Footer from './component/5-Footer/Footer'
+function App() {
+
+
+  return (
+ <div>
+  <Header/>
+  <Hero/>
+  <Main/>
+  <Contact/>
+  <Footer/>
+ </div>
+     
+  
+  )
+}
+
+export default App
---
+++ Portfolio/src/component/2-Hero/hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './hero.css'
+export default function hero() {
+  return (
+    <div>
+      Hero
+    </div>
+  )
+}
